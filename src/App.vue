e2efc0f56c4f7e523f83810299e1b849f434217c
WormNimaya <3344800699@qq.com>
2020-05-22 16:45:28 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios' 
+// import jsonp from 'jsonp'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    axios.get('/api/activity/servicetime').then((data) =>{
+      console.log(data.data)
+    })
+      //  let url = 'https://www.imooc.com/activity/servicetime'
+      //  jsonp(url, (err, res) => {
+      //    console.log(res)
+      //  })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
